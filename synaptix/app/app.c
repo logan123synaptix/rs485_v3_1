07e0acc31ddd90365edc8b5d99e3c47b9424d42a
ngoxuanloc2309 <ngoxuanloc23092004@gmail.com>
2026-07-06 16:22:15 +0700

    fix bug

--- synaptix/app/app.c
+++ synaptix/app/app.c
@@ -14,18 +14,23 @@ static const osThreadAttr_t app_task_attr = {
     .priority   = APP_TASK_PRIORITY
 };
 
-BSP_UART_t s_bsp_uart;
-
 static void uart_log_write(const char *s){
-    bsp_uart_transmit(s_bsp_uart.BSP_LOGGER, (uint8_t *)s, strlen(s), 100);
+    bsp_uart_transmit(BSP_LOGGER, (uint8_t *)s, strlen(s), 100);
 }
 
 static void app_logger_init(void){
-    bsp_uart_init(s_bsp_uart.BSP_LOGGER);
+    bsp_uart_init(BSP_LOGGER);
     logger_init(LOGGER_DEBUG, uart_log_write);
     LOGI(TAG, "Logger Init Success");
 }
 
 void app_init(void){
     app_logger_init();
+
+    /*another initialize*/
+}
+
+void app_task(void *arg){
+    (void)arg;
+    log_info(TAG, "APP TASK IS CREATING");
 }
