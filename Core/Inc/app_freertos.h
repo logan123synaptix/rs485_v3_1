666b8e531604d48954b40d744708b76750aabad9
ngoxuanloc2309 <ngoxuanloc23092004@gmail.com>
2026-07-07 18:23:21 +0700

    configUSE_MALLOC_FAILED_HOOK || stack overflow

--- Core/Inc/app_freertos.h
+++ Core/Inc/app_freertos.h
@@ -65,6 +65,9 @@ void StartDefaultTask(void *argument);
 
 void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
 
+/* Hook prototypes */
+void vApplicationMallocFailedHook(void);
+void vApplicationStackOverflowHook(xTaskHandle xTask, char *pcTaskName);
 /* Private application code --------------------------------------------------*/
 /* USER CODE BEGIN Application */
 
